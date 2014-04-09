3ab4b1d28fb73b7e6cad3fb4826780345ebdb3cf
udv.my.projects <udv.my.projects@gmail.com>
2014-04-09 13:35:17 +0000

    First commit

---
+++ LeagueSoldierDeathTeam/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using LeagueSoldierDeathTeam.Models.Account;
+
+namespace LeagueSoldierDeathTeam.Controllers
+{
+	public class AccountController : Controller
+	{
+		[HttpGet]
+		public ActionResult LogOn()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult LogOn(LogOnModel model)
+		{
+			return View();
+		}
+	}
+}
---
+++ LeagueSoldierDeathTeam/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace LeagueSoldierDeathTeam.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
