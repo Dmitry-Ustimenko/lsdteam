6c064ec48e90176903c3e2bb2e3b3c93a994cf74
udv.my.projects@gmail.com <udv.my.projects@gmail.com@044480e3-4e80-b5c5-8523-540e9e6e491d>
2014-04-17 14:33:23 +0000

    Slyle implementation

--- trunk/LeagueSoldierDeathTeam.Site/Controllers/HomeController.cs
+++ trunk/LeagueSoldierDeathTeam.Site/Controllers/HomeController.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Web.Mvc;
-using LeagueSoldierDeathTeam.BusinessLogic.Abstractions.Factories;
+﻿using System.Web.Mvc;
 
 namespace LeagueSoldierDeathTeam.Site.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ServiceFactoryBase _serviceFactory;
-
-		public HomeController(ServiceFactoryBase serviceFactory)
-		{
-			if (serviceFactory == null)
-				throw new ArgumentNullException();
-			_serviceFactory = serviceFactory;
-		}
-
 		public ActionResult Index()
 		{
 			return View();
