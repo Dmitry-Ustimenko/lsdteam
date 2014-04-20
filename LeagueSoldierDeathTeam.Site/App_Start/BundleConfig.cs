462fba6ec414b881a154586dd58ad4d39858e5c4
udv.my.projects <udv.my.projects@gmail.com>
2014-04-20 11:56:20 +0000

    refactoring

--- LeagueSoldierDeathTeam.Site/App_Start/BundleConfig.cs
+++ LeagueSoldierDeathTeam.Site/App_Start/BundleConfig.cs
@@ -7,15 +7,20 @@ namespace LeagueSoldierDeathTeam.Site.App_Start
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js",
-						"~/Scripts/jquery.validate*",
-						"~/Scripts/modernizr-*"));
+				"~/Scripts/jquery-{version}.js",
+				"~/Scripts/jquery.validate*",
+				"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/js").Include(
-					"~/Scripts/Plugins/jssor.core.js",
-					"~/Scripts/Plugins/jssor.utils.js",
-					"~/Scripts/Plugins/jssor.slider.js"
-				));
+				"~/Scripts/Plugins/jssor.core.js",
+				"~/Scripts/Plugins/jssor.utils.js",
+				"~/Scripts/Plugins/jssor.slider.js",
+
+				"~/Scripts/Site/site.js",
+				"~/Scripts/Site/site.ajax.js",
+				"~/Scripts/Site/site.plugins.js",
+				"~/Scripts/Site/site.common.js",
+				"~/Scripts/Site/site.home.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 				"~/Content/Plugins/slider.css",
