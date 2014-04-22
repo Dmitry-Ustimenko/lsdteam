4880d5b6a698275996731bec973bb33dd45cc24f
udv.my.projects <udv.my.projects@gmail.com>
2014-04-22 13:52:44 +0000

    rou

--- LeagueSoldierDeathTeam.Site/App_Start/RouteConfig.cs
+++ LeagueSoldierDeathTeam.Site/App_Start/RouteConfig.cs
@@ -8,6 +8,11 @@ namespace LeagueSoldierDeathTeam.Site.App_Start
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("scripts/{*pathinfo}");
+			routes.IgnoreRoute("content/{*pathinfo}");
+			routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+
 			routes.LowercaseUrls = true;
 			routes.MapMvcAttributeRoutes();
 
