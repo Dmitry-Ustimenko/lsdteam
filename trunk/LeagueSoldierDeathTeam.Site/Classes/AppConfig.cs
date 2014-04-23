2f8611b1f13cfb55b53b7aadbc878fd041a16064
udv.my.projects@gmail.com <udv.my.projects@gmail.com@044480e3-4e80-b5c5-8523-540e9e6e491d>
2014-04-23 14:37:19 +0000

    AuthenticationManager

--- trunk/LeagueSoldierDeathTeam.Site/Classes/AppConfig.cs
+++ trunk/LeagueSoldierDeathTeam.Site/Classes/AppConfig.cs
@@ -6,5 +6,7 @@ namespace LeagueSoldierDeathTeam.Site.Classes
 	public static class AppConfig
 	{
 		public static string HostName { get { return ConfigurationManager.AppSettings["Host.Name"]; } }
+
+		public static string CookieName { get { return ConfigurationManager.AppSettings["Cookie.Name"]; } }
 	}
 }
