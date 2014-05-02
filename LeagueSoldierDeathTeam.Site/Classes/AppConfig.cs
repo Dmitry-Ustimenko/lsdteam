3a5a3ed6ebc42c9415acc1de252e41f2bf6fdb94
udv.my.projects <udv.my.projects@gmail.com>
2014-05-02 13:52:15 +0000

    mailer added

--- LeagueSoldierDeathTeam.Site/Classes/AppConfig.cs
+++ LeagueSoldierDeathTeam.Site/Classes/AppConfig.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
 
 namespace LeagueSoldierDeathTeam.Site.Classes
 {
 	public static class AppConfig
 	{
+		public static string SystemEmailAddress { get { return ConfigurationManager.AppSettings["NoreplyEmail"]; } }
+		
+		public static string SystemEmailName { get { return ConfigurationManager.AppSettings["NoreplyName"]; } }
+
 		public static string HostName { get { return ConfigurationManager.AppSettings["Host.Name"]; } }
 
 		public static string CookieName { get { return ConfigurationManager.AppSettings["Cookie.Name"]; } }
+
+		public static string MailAdmin { get { return ConfigurationManager.AppSettings["Mail.Admin"]; } }
+
+		public static bool MailIsDebug { get { return bool.Parse(ConfigurationManager.AppSettings["Mail.IsDebug"]); } }
 	}
 }
