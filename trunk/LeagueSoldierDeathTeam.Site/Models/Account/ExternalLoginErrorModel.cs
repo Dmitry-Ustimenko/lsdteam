1871623a691f10ec6abe24cf2c35a49295f6d339
udv.my.projects@gmail.com <udv.my.projects@gmail.com@044480e3-4e80-b5c5-8523-540e9e6e491d>
2014-05-03 12:49:21 +0000

    External login error implementation

---
+++ trunk/LeagueSoldierDeathTeam.Site/Models/Account/ExternalLoginErrorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeagueSoldierDeathTeam.Site.Models.Account
+{
+	public class ExternalLoginErrorModel
+	{
+		public string Message { get; set; }
+
+		public string ProviderName { get; set; }
+	}
+}
